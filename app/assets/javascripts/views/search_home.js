e6bbe99e1af9d78c1160d32af4027cdaa8ff9dc1
kaylindris <yt7739@gmail.com>
2014-05-23 09:11:37 -0700

    day 3 start

--- app/assets/javascripts/views/search_home.js
+++ app/assets/javascripts/views/search_home.js
@@ -2,6 +2,35 @@ window.Bookfriends.Views.SearchHome = Backbone.CompositeView.extend({
 
   template: JST["search/home"],
 
+  initialize: function() {
+    this.shelf = new Bookfriends.Models.Bookshelf();
+
+    // TODO: Add initialization logic to this here and the router to set up another bookshelf
+    // that contains not only all your books but also all your friends books.
+    // Do this in order to match up all the books you and your friends have
+    // against google API search results.
+
+    this.listenTo(this.shelf.books(), "sync", this.render);
+    this.listenTo(this.shelf.books(), "add", this.addBook);
+    this.listenTo(this.shelf.books(), "remove", this.removeBook);
+  },
+
+  addBook: function(book) {
+    var bookShowView = new Bookfriends.Views.BookShow({
+      model: book
+    });
+    this.addSubview("#main-search-results", bookShowView);
+  },
+
+  removeBook: function(book) {
+    var subview = _.find(this.subviews("#main-search-results"),
+      function(subview) {
+        return subview.model === book;
+      }
+    );
+    this.removeSubview("#main-search-results", subview);
+  },
+
   events: {
     "keyup #main-search-box": "keyUpHandler"
   },
@@ -11,6 +40,8 @@ window.Bookfriends.Views.SearchHome = Backbone.CompositeView.extend({
 
     this.$el.html(renderedContent);
 
+    this.attachSubviews();
+
     return this;
   },
 
@@ -19,7 +50,7 @@ window.Bookfriends.Views.SearchHome = Backbone.CompositeView.extend({
       this.performSearch(event);
       return;
     }
-
+    // http://suggestqueries.google.com/complete/search?client=books&ds=bo&q=monxxxxx
   },
 
   performSearch: function(event) {
@@ -39,7 +70,31 @@ window.Bookfriends.Views.SearchHome = Backbone.CompositeView.extend({
   },
 
   handleSearchResponse: function(response) {
-    console.log(response);
+    var parsedBooks = this.extractBooksData(response);
+    console.log(parsedBooks);
+    this.shelf.books().set(parsedBooks);
+  },
+
+  extractBooksData: function(response) {
+    var results = [];
+    response.items.forEach(function(item) {
+      var volume = {};
+      _(volume).extend({
+        google_id: item["id"] ,
+        title: item["volumeInfo"]["title"] || "",
+        subtitle: item["volumeInfo"]["subtitle"],
+        author: item["volumeInfo"]["authors"] ? item["volumeInfo"]["authors"][0] : "",
+        selfLink: item["selfLink"],
+        thumbnail: item["volumeInfo"]["imageLinks"] ?
+            item["volumeInfo"]["imageLinks"]["thumbnail"] : undefined,
+        smallThumbnail: item["volumeInfo"]["imageLinks"] ? 
+            item["volumeInfo"]["imageLinks"]["smallThumbnail"] : undefined
+      });
+      results.push(volume);
+    });
+    return results;
   }
+
+
 
 });
